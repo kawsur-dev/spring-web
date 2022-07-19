5388595617861a6dc943805b0061ac2592460af6
MIRMDKAWSUR <kawsur@aiub.edu>
2022-07-19 09:15:58 +0600

    init spring mvc (legacy)

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class DashboardController {
+
+    @RequestMapping("/dashboard")
+    public String show() {
+        return "dashboard";
+    }
+
+    @RequestMapping("/process/v1")
+    public String processOne() {
+        return "confirmation";
+    }
+
+    @RequestMapping("/process/v2")
+    public String processTwo(HttpServletRequest request, Model model) {
+       String fullname = request.getParameter("fullname");
+       if (! fullname.isEmpty()) {
+           fullname = fullname.toUpperCase();
+       }
+       model.addAttribute("username", fullname);
+       return "confirmation";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/dashboard.jsp
@@ -0,0 +1,27 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: MIRMDKAWSUR
+  Date: 7/19/2022
+  Time: 8:44 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+<h1>Welcome to Dashboard!</h1>
+
+<%--<form action="process/v1" method="post">
+    Please enter your name: <input type="text" name="fullname">
+    <input type="submit" name="submit">
+</form>--%>
+
+<form action="process/v2" method="post">
+    Please enter your name: <input type="text" name="fullname">
+    <input type="submit" name="submit">
+</form>
+
+</body>
+</html>
