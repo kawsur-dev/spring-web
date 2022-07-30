5c2f6a63bf74b9844ee3105abc1b0bcd916f0e46
kawsurilu <kawsurilu@yahoo.com>
2022-07-30 22:59:44 +0600

    java config

---
+++ src/main/java/com/config/AppConfig.java
@@ -0,0 +1,65 @@
+package com.config;
+
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"com.dao","com.service","com.controller"})
+public class AppConfig implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("/css");
+    }
+
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
+    public DataSource dataSource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setDatabaseName("hibernate_tutorial");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/hibernate_tutorial");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    public SessionFactory sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.model");
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("show_sql", "true");
+        sessionFactory.setHibernateProperties(properties);
+        return sessionFactory.getObject();
+    }
+
+    public PlatformTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory());
+        return transactionManager;
+    }
+}
--- src/main/java/com/dao/UserContactDaoImpl.java
+++ src/main/java/com/dao/UserContactDaoImpl.java
@@ -1,10 +1,10 @@
 package com.dao;
 
 import com.model.UserContact;
-import com.model.UserDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
